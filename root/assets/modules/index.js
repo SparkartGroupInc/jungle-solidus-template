b8d59c9cc852c3c6e92e9dcbedc2db1df5d31e90
Josiah Sprague <josiah.sprague@gmail.com>
2014-12-08 21:36:14 -0500

    configure mailing list service dynamically
    configure mailing list service dynamically based on the presence of a MailChimp vs. Universe key in the site globals

--- root/assets/modules/index.js
+++ root/assets/modules/index.js
@@ -8,17 +8,26 @@ if (window.addEventListener) {
   window.attachEvent('onload', initWidgets);
 }
 
+var mailingListConfig = {template: subscribeTemplate};
+
+if (site.integrations.sendgrid_apikey) {
+  mailingListConfig.service = 'sendgrid';
+  mailingListConfig.key = site.integrations.sendgrid_apikey;
+} else if (site.integrations.mailchimp_url) {
+  mailingListConfig.service = 'mailchimp';
+  mailingListConfig.url = site.integrations.mailchimp_url;
+} else {
+  mailingListConfig.service = 'universe';
+  mailingListConfig.key = site.integrations.universe_apikey;
+}
+
 function initWidgets() {
 
   var subscribePlaceholders = document.querySelectorAll('.widget-subscribe .subscribe-placeholder');
 
   Array.prototype.forEach.call(subscribePlaceholders, function(placeholder){
-    var mySubscribeForm = new SubscribeEmail({
-      element: placeholder,
-      service: 'sendgrid',
-      key: site.integrations.mailinglist_apikey,
-      template: subscribeTemplate
-    });
+    mailingListConfig.element = placeholder;
+    var mySubscribeForm = new SubscribeEmail(mailingListConfig);
   });
 
 };
